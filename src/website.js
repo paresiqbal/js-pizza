4050b9d8d10cdc5c238f8e367acab0d0d43f8a3b
paresiqbal <thecrowdel@gmail.com>
2023-02-18 23:27:24 +0700

    Adding menu and fixing another stuff

--- src/website.js
+++ src/website.js
@@ -1,58 +1,99 @@
+import loadHome from "./home";
+import loadMenu from "./menu";
+// import loadContact from "./contact";
+
 function createHeader() {
   const header = document.createElement("header");
   header.classList.add("header");
 
   const restaurantName = document.createElement("h1");
   restaurantName.classList.add("restaurant-name");
-  restaurantName.textContent = "Just Ordinary Restaurant";
+  restaurantName.textContent = "Pizza Time";
 
   header.appendChild(restaurantName);
-  header.appendChild(createNavbar());
+  header.appendChild(createNav());
 
   return header;
 }
 
-function createNavbar() {
-  const navbar = document.createElement("nav");
+function createNav() {
+  const nav = document.createElement("nav");
 
   const homeButton = document.createElement("button");
   homeButton.classList.add("button-nav");
   homeButton.textContent = "Home";
-
   homeButton.addEventListener("click", (e) => {
     if (e.target.classList.contains("active")) return;
     setActiveButton(homeButton);
+    loadHome();
   });
 
   const menuButton = document.createElement("button");
   menuButton.classList.add("button-nav");
   menuButton.textContent = "Menu";
-
   menuButton.addEventListener("click", (e) => {
     if (e.target.classList.contains("active")) return;
     setActiveButton(menuButton);
+    loadMenu();
   });
 
   const contactButton = document.createElement("button");
   contactButton.classList.add("button-nav");
   contactButton.textContent = "Contact";
-
   contactButton.addEventListener("click", (e) => {
     if (e.target.classList.contains("active")) return;
     setActiveButton(contactButton);
+    loadContact();
   });
 
-  navbar.appendChild(homeButton);
-  navbar.appendChild(menuButton);
-  navbar.appendChild(contactButton);
+  nav.appendChild(homeButton);
+  nav.appendChild(menuButton);
+  nav.appendChild(contactButton);
 
-  return navbar;
+  return nav;
+}
+
+function setActiveButton(button) {
+  const buttons = document.querySelectorAll(".button-nav");
+
+  buttons.forEach((button) => {
+    if (button !== this) {
+      button.classList.remove("active");
+    }
+  });
+
+  button.classList.add("active");
+}
+
+function createMain() {
+  const main = document.createElement("main");
+  main.classList.add("main");
+  main.setAttribute("id", "main");
+  return main;
+}
+
+function createFooter() {
+  const footer = document.createElement("footer");
+  footer.classList.add("footer");
+
+  const copyright = document.createElement("p");
+  copyright.textContent = `Copyright © ${new Date().getFullYear()} pares`;
+
+  githubLink.appendChild(githubIcon);
+  footer.appendChild(copyright);
+
+  return footer;
 }
 
 function initializeWebsite() {
   const content = document.getElementById("content");
 
   content.appendChild(createHeader());
+  content.appendChild(createMain());
+  content.appendChild(createFooter());
+
+  setActiveButton(document.querySelector(".button-nav"));
+  loadHome();
 }
 
 export default initializeWebsite;
